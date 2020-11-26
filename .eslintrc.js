5db458a5c3c2fa9d9f93db2c21678e1cd43644bb
Joaquim Neto <joaquimmy@yahoo.com>
2020-11-26 09:12:37 -0300

    yarn 2 is not developer friendly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,27 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
-    es2020: true,
     node: true,
+    es2021: true,
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      impliedStrict: true,
-    },
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
     'no-shadow': ['error', { allow: ['_'] }],
     'no-use-before-define': ['error', { functions: false }],
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/explicit-module-boundary-types': [
+      'warn',
+      {
+        allowedNames: ['handler'],
+        allowArgumentsExplicitlyTypedAsAny: true
+      },
+    ],
   },
 };
